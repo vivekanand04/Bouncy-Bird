1fa52e5f8f5c56002fba6c5f02ee5a2e58d604c8
vivekannd <vivekanandath04@gmail.com>
2025-04-08 19:26:42 +0530

    commit

---
+++ flappybird.js
@@ -0,0 +1,182 @@
+// board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+// bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 5; // Shifted to right
+let birdY = boardHeight / 2;
+let birdImg;
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
+
+// pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+// physics
+let velocityX = -2;
+let velocityY = 0;
+let gravity = 0.4;
+
+let gameOver = false;
+let score = 0;
+let gameStarted = false;
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d");
+
+  birdImg = new Image();
+  birdImg.src = "./flappybird.png";
+  birdImg.onload = function () {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  };
+
+  topPipeImg = new Image();
+  topPipeImg.src = "./toppipe.png";
+
+  bottomPipeImg = new Image();
+  bottomPipeImg.src = "./bottompipe.png";
+
+  requestAnimationFrame(update);
+  setInterval(placePipes, 1500);
+  document.addEventListener("keydown", moveBird);
+};
+
+function update() {
+  requestAnimationFrame(update);
+  context.clearRect(0, 0, board.width, board.height);
+
+  // If game hasn't started yet
+  if (!gameStarted) {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    context.fillStyle = "white";
+    context.font = "20px sans-serif";
+    context.fillText(
+      "Press Space to Start",
+      boardWidth / 4.5,
+      boardHeight / 2 + 50
+    );
+    return;
+  }
+
+  if (gameOver) {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    context.fillStyle = "white";
+    context.fillRect(30, 80, 300, 50); // White background
+    context.fillStyle = "black";
+    context.font = "bold 40px sans-serif";
+    context.fillText("GAME OVER", 60, 115);
+    return;
+  }
+
+  // Bird movement
+  velocityY += gravity;
+  bird.y = Math.max(bird.y + velocityY, 0);
+  context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+  if (bird.y > board.height) {
+    gameOver = true;
+  }
+
+  // Pipes
+  for (let i = 0; i < pipeArray.length; i++) {
+    let pipe = pipeArray[i];
+    pipe.x += velocityX;
+    context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+    if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+      score += 0.5;
+      pipe.passed = true;
+    }
+
+    if (detectCollision(bird, pipe)) {
+      gameOver = true;
+    }
+  }
+
+  // Remove off-screen pipes
+  while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+    pipeArray.shift();
+  }
+
+  // Score
+  context.fillStyle = "white";
+  context.font = "45px sans-serif";
+  context.fillText(score, 5, 45);
+}
+
+function placePipes() {
+  if (gameOver || !gameStarted) {
+    return;
+  }
+
+  let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+  let openingSpace = board.height / 4;
+
+  let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: randomPipeY,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+  pipeArray.push(topPipe);
+
+  let bottomPipe = {
+    img: bottomPipeImg,
+    x: pipeX,
+    y: randomPipeY + pipeHeight + openingSpace,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+  pipeArray.push(bottomPipe);
+}
+
+function moveBird(e) {
+  if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+    if (!gameStarted) {
+      gameStarted = true;
+      return;
+    }
+
+    velocityY = -6;
+
+    if (gameOver) {
+      bird.y = birdY;
+      pipeArray = [];
+      score = 0;
+      velocityY = 0;
+      gameOver = false;
+      gameStarted = false;
+    }
+  }
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
